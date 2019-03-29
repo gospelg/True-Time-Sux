db07192095d49a52c64d928cb4b11287633af0d9
gospelg <garrettcrosby@outlook.com>
2019-03-29 13:21:48 -0400

    update 1.2
    fixed typos

--- install.py
+++ install.py
@@ -1,35 +1,34 @@
 from cryptography.fernet import Fernet
 from subprocess import call
 import os
-import ConfigParser
+import configparser
 
 def create_config(user, password, cwd):
-    webiste = 'https://unionskyward.nefec.org:444/scripts/wsisa.dll/WService=wsFin/seplog01.w'
-	filepath = cwd + 'setup.ini'
-    if os.path.isfile(filepath):
-        with open(filepath, 'w') as f:
-            Config.set('GENERAL', 'password', password)
-	else:    
-        with open(filepath, 'w') as f:
-	        Config.add_section('GENERAl')
-		    Config.set('GENERAL', 'driver', '{0}chromedriver.exe'.format(cwd))
-            Config.set('GENERAL', 'website', website)		
-            Config.set('GENERAL', 'user', user)
-		    Config.set('GENERAL', 'password', password)
-		    Config.write(f)
-        
+    Config = configparser.ConfigParser()
+    website = 'https://unionskyward.nefec.org:444/scripts/wsisa.dll/WService=wsFin/seplog01.w'
+    filepath = cwd + 'setup.ini'
+    with open(filepath, 'w') as f:
+        Config.add_section('GENERAL')
+        Config.set('GENERAL', 'driver', '{0}chromedriver.exe'.format(cwd))
+        Config.set('GENERAL', 'website', website)
+        Config.set('GENERAL', 'user', user)
+        Config.set('GENERAL', 'password', password)
+        Config.write(f)
+
+#def create_task(cwd):
+
+
 def main():
     cwd = os.getcwd()
-    key = '' #insert encryption key here. Should match the key in the code of main file.
+    key = 'l2GYHWhRkv6rrPgbGGvbAsxQpNo4QF4tNtu1QWEg3uE='
     cipher_suite = Fernet(key)
-	username = input('What is your Skyward login username?\n')
-	password = input('What is your password?\n')
-	password_encrypt = cipher_suite.encrypt(password)
-	
-	try:
-	    create_config(user, password, cwd)
-	except:
-	    print 'Failed to create config file.'
-	
-main()
+    username = input('What is your Skyward login username?\n')
+    password = input('What is your password?\n')
+    password_encrypt = cipher_suite.encrypt(password.encode('ascii'))
+    try:
+        create_config(username, password, cwd)
+    except Exception as ex:
+        print('Failed to create config file.')
+        print(ex)
 
+main()
